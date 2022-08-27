a3c5fa5f079bb86d50875b4a9fa758c0d1155b32
vaavaav <pedro_peixoto@outlook.pt>
2022-08-27 23:21:17 +0100

    Update config files

--- vaavaav/home.nix
+++ vaavaav/home.nix
@@ -6,14 +6,6 @@
   home.username = "vaavaav";
   home.homeDirectory = "/home/vaavaav";
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "22.05";
 
   home.packages = with pkgs; [
