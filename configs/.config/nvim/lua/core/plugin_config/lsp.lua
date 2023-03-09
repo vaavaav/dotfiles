10c175766154f3383ff6ca719b9fba901bcbdfbd
vaavaav <vaavaav.pt@gmail.com>
2023-03-09 18:44:50 +0000

    Require list of lsp servers

---
+++ configs/.config/nvim/lua/core/plugin_config/lsp.lua
@@ -0,0 +1,24 @@
+local lsp = require('lsp-zero').preset({
+  name = 'minimal',
+  set_lsp_keymaps = true,
+  manage_nvim_cmp = true,
+  suggest_lsp_servers = false,
+})
+
+-- (Optional) Configure lua language server for neovim
+lsp.nvim_workspace()
+
+lsp.ensure_installed({
+    'rust_analyzer',
+    'ltex',
+    'clangd',
+    'grammarly',
+    'dhall_lsp_server',
+    'ansiblels',
+    'cmake',
+    'elixirls',
+    'jsonls',
+    'yamlls',
+})
+
+lsp.setup()
