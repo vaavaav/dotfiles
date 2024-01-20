ebfdb7496119262ee06a7c25dd8c5904fa40d7e3
vaavaav <vaavaav.pt@gmail.com>
2024-01-20 17:05:12 +0000

    Add some primeogen keymaps

--- configs/.config/nvim/lua/core/keymaps.lua
+++ configs/.config/nvim/lua/core/keymaps.lua
@@ -5,7 +5,13 @@ vim.o.shiftwidth = 2
 vim.o.smarttab = true
 vim.o.expandtab = true
 vim.o.hlsearch = false
+vim.g.mapleader = ' '
 
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>', {noremap = true})
 vim.keymap.set('n', '<c-f>', ':FZF<CR>', {noremap = true})
 vim.keymap.set('n', '<c-t>', ':ToggleTerm<CR>', {noremap = true})
+vim.keymap.set({'n', 'v'}, '<c-u>', '<c-u>zz')
+vim.keymap.set({'n', 'v'}, '<c-d>', '<c-d>zz')
+vim.keymap.set({'n', 'v'}, 'n', 'nzz')
+vim.keymap.set({'n', 'v'}, 'N', 'Nzz')
+vim.keymap.set({'n', 'v'}, '<Leader>p', '"_dP')
